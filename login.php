fb4bdbcadaf9120253c8d24ebb249db669d9d78a
s.ausili <stefano.ausili@poliziadistato.it>
2025-10-17 12:28:10 +0200

    CORS

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 header('Content-Type: application/json');
 require_once __DIR__ . '/utils.php';
 
