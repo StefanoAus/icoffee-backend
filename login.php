ec237326481becb2dead221ddfe679e15592a56c
s.ausili <stefano.ausili@poliziadistato.it>
2025-10-17 12:31:10 +0200

    ciao

--- login.php
+++ login.php
@@ -2,8 +2,13 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 header('Content-Type: application/json');
 require_once __DIR__ . '/utils.php';
